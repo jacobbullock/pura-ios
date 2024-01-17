ed73ddead69b478efe977b79bd0ae3303bab80ca
Jacob Bullock <jacob.bullock@gmail.com>
2024-01-17 07:59:10 -0800

    add support for localized descriptions in errors

--- SampleApp/API/APIError.swift
+++ SampleApp/API/APIError.swift
@@ -15,3 +15,15 @@ enum APIError: Error {
     case emptyQuery
     case tooShort(String)
 }
+
+extension APIError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .badURL: return "The request url is not formed properly, The programmers will now have to submit a new build and wait for apple approve it. It's a very daunting process"
+        case .custom(let str): return str
+        case .noData: return "There was no data for this word"
+        case .emptyQuery: return "Please enter a word to search"
+        case .tooShort: return "Please enter a word that is at least 3 characters. We assume that you already know all the definitions of all two letter words."
+        }
+    }
+}
